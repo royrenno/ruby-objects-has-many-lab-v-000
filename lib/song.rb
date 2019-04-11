feeed3e62e7249f75e33b71d4cf5fe47e2046799
roy renno <royrenno@yahoo.com>
2019-04-11 04:07:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,14 @@
 class song
   attr_accessor :artist, :name
-  
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if self.artist
+      self.artist_name
+    else 
+      nil
+    end
+  end
+end
