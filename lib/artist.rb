4f4551f20d3ff93596516a242b62758d6fc8908a
roy renno <royrenno@yahoo.com>
2019-04-11 04:18:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name
+  @@song.count = 0
+  def initialize(name)
+    @name = name
+    @song =[]
+  end
+  def add_song(song)
+    self.artist = self
+    @@song.count+=1
+  end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song.count +=1
+  end
+  def songs
+    @songs
+  end
+  def self.song_count
+    @@song_count
+  end 
+end
